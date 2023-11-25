e6bafbfa4ab02b388b747d9d3706268d89da88b3
Kyle G. Velez <kyleg.velez@gmail.com>
2023-11-26 02:59:27 +0800

    profile

--- news_app/lib/pages/profile.dart
+++ news_app/lib/pages/profile.dart
@@ -53,6 +53,57 @@ class _ProfileState extends State<Profile> {
     );
   }
 
+  Widget _buildInterestCard(
+      {IconData? iconData, String? label, Color? bgColor, String? imageUrl}) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            spreadRadius: 1,
+            blurRadius: 3,
+            offset: Offset(0, 3),
+          ),
+        ],
+        border: Border.all(
+            style: BorderStyle.solid, width: 1.0, color: Colors.black12),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      margin: EdgeInsets.all(3.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (imageUrl != null)
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(color: Colors.black54, width: 1),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  imageUrl,
+                  width: 24,
+                  height: 24,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          if (iconData != null && imageUrl == null)
+            Icon(iconData, color: Colors.white),
+          SizedBox(width: 10),
+          Text(
+            label ?? '',
+            style: TextStyle(
+                color: Colors.white, fontSize: 12, fontWeight: FontWeight.w700),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -123,6 +174,150 @@ class _ProfileState extends State<Profile> {
               onTap: () {},
             ),
           ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15.0),
+          color: Colors.white,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: Icon(Icons.settings),
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Settings()));
+                  },
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('images/dev2.png'),
+                ),
+              ),
+              SizedBox(height: 40),
+              Text(
+                'Mary Eddythe S. Sornito',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 50), //end of profile
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Your Interests',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        _buildInterestCard(
+                            iconData: Icons.computer,
+                            label: 'Technology',
+                            bgColor: Color.fromARGB(255, 22, 32, 163)),
+                        _buildInterestCard(
+                            imageUrl:
+                                "https://st3.depositphotos.com/1071909/19574/i/450/depositphotos_195745476-stock-photo-artificial-intelligence-ai.jpg",
+                            label: 'AI',
+                            bgColor: Color.fromARGB(255, 104, 11, 135)),
+                        _buildInterestCard(
+                            imageUrl:
+                                'https://media.istockphoto.com/id/1252858062/photo/beautiful-woman-wearing-stylish-sunglasses.jpg?s=612x612&w=0&k=20&c=Mn2QI1jobjZur6GCS19KNkencE72yMDPR560leEjWm4=',
+                            label: 'Fashion',
+                            bgColor: Color.fromARGB(255, 154, 23, 23)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20), //end of interests section
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Your Recently Read',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Icon(Icons.book),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'You have no reading history yet. ',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton.icon(
+                onPressed: () {},
+                label: Text('Support the Edithor.ial'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF001747),
+                  foregroundColor: Colors.white,
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                ),
+                icon: Icon(Icons.arrow_forward),
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: buildBottomNavigationBar(context),
